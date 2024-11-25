51d8785e6138989de78b0f0c645abd41d064ca7e
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-11-25 16:46:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 //your JS code here. If required.
 const promise1 = (arr) => new Promise(resolve =>{
-	setTimeout(() => resolve(arr,3000));	
-}).then(function(result){
-	let evenarr =[];
-	result.forEach((element) =>{
-		if(element%2==0){
-			evenarr.push(element);
-		}
+	setTimeout(() => {resolve(arr);},3000);	
+}).then(arr => {
+	return arr.filter(num => num%2===0)
 	});
 	return evenarr;
-}).then(function(result1){
-	result1.forEach((element) => {
-		element = element * 2;
-	});
-	document.getElemenetById("output").innerHTML = result1;
+}).then(arr => {
+	return arr.map(num => num*2);
+	
 });
 
 let arr = [1,2,3,4];
-promise1([1,2,3,4]);
+promise1([1,2,3,4]).then(result3 => {
+	document.getElemenetById("output").innerHTML = result1;
+});
